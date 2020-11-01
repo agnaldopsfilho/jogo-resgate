9e8d94f46a108ebf3815bf802c041f29b6c0c6b1
Agnaldo26 <agnaldosilva9704@gmail.com>
2020-10-31 22:40:08 -0300

    Adicionado div #fim ao acabar a energia do jogador

--- js/js.js
+++ js/js.js
@@ -449,9 +449,30 @@ function energia() {
 			$("#energia").css("background-image", "url(imgs/energia0.png)");
 			
 			//Game Over
+			gameOver()
 		}
 	
 	} // Fim da função energia()
+
+	//Função GAME OVER
+	function gameOver() {
+		fimdejogo=true;
+		musica.pause();
+		somGameover.play();
+		
+		window.clearInterval(jogo.timer);
+		jogo.timer=null;
+		
+		$("#jogador").remove();
+		$("#inimigo1").remove();
+		$("#inimigo2").remove();
+		$("#amigo").remove();
+		
+		$("#fundoGame").append("<div id='fim'></div>");
+		
+		$("#fim").html(`<h1> Game Over </h1> <p> Sua pontuação foi: ${pontos} </p> <div id='reinicia' onClick=reiniciaJogo()> <h3> Jogar Novamente</h3></div>`);
+		} // Fim da função gameOver();
+	
 
 } // Fim da função start
 
