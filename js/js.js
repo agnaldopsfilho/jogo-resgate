d4b7aa859cee025c529b4962692b68e6252f0063
Agnaldo26 <agnaldosilva9704@gmail.com>
2020-10-31 22:13:12 -0300

    Criado e estilizado a barra de energia

--- js/js.js
+++ js/js.js
@@ -6,13 +6,15 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energia'></div>");
 
 
 	//Principais variáveis do jogo
 	var pontos=0;
 	var salvos=0;
 	var perdidos=0;
-	
+	var energiaAtual=3;
+
 	var podeAtirar=true;
 	var fimdejogo=false;	
 	var jogo = {}
@@ -57,7 +59,7 @@ moveinimigo2();
 moveamigo();
 colisao();
 placar()
-
+energia()
 
 } // Fim da função loop()
 
@@ -195,6 +197,8 @@ function colisao() {
 			
 		if (colisao1.length>0) {
 			
+			energiaAtual--;
+			
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
 			explosao1(inimigo1X,inimigo1Y) ;
@@ -207,6 +211,8 @@ function colisao() {
 		
 			// jogador com o inimigo2 
 		if (colisao2.length>0) {
+			
+			energiaAtual--;
 	
 			inimigo2X = parseInt($("#inimigo2").css("left"));
 			inimigo2Y = parseInt($("#inimigo2").css("top"));
@@ -398,7 +404,36 @@ function explosao3(amigoX,amigoY) {
 		
 	} //fim da função placar()
 
+	//Barra de energia
+function energia() {
+	
+		if (energiaAtual==3) {
+			
+			$("#energia").css("background-image", "url(imgs/energia3.png)");
+		}
+	
+		if (energiaAtual==2) {
+			
+			$("#energia").css("background-image", "url(imgs/energia2.png)");
+		}
+	
+		if (energiaAtual==1) {
+			
+			$("#energia").css("background-image", "url(imgs/energia1.png)");
+		}
+	
+		if (energiaAtual==0) {
+			
+			$("#energia").css("background-image", "url(imgs/energia0.png)");
+			
+			//Game Over
+		}
+	
+	} // Fim da função energia()
+
 } // Fim da função start
+
+
 
 
 
